6a1a309171c217dcf8e7af4340662e37ac0e705b
Shiloh Rodrigues <shilohrodrigues@hotmail.com>
2024-01-20 17:57:02 -0500

    Form changes, merging commits

--- form.js
+++ form.js
@@ -1,54 +1,3 @@
-//For header scroll animation
-let lastScrollTop = 0;
-// Get the header element
-const header = document.querySelector("header");
-const headerStyles = window.getComputedStyle(header);
-// Add a scroll event listener to the window object
-window.addEventListener("scroll", () => {
-  const currentScrollTop = window.pageYOffset;
-  if (currentScrollTop > lastScrollTop) {
-    // User is scrolling down
-    header.style.top = `-${headerStyles.getPropertyValue("height")}`; // Adjust the value based on the height of your header
-  } else {
-    // User is scrolling up
-    header.style.top = "0";
-  }
-  lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop; // For Mobile or negative scrolling
-});
-
-//For hamburger menu functions
-const openMenu = document.getElementById("hamburgerMenu");
-const mobileNav = document.getElementById("mobileNav");
-const mobileNavStyles = window.getComputedStyle(mobileNav);
-const mainContent = document.getElementById("gridder");
-let mobileNavOpen = false;
-mobileNav.style.top = `${headerStyles.getPropertyValue("height")}`;
-
-openMenu.addEventListener("click", openMobileNav); 
-function openMobileNav() {
-  if (mobileNavOpen) {
-    openMenu.classList.remove('close-icon');
-    mobileNav.classList.remove('open-nav');
-    mainContent.style.transform = `translateY(0)`;
-    mobileNavOpen = false;
-  }
-  else {
-    openMenu.classList.add('close-icon');
-    mobileNav.classList.add('open-nav');
-    mainContent.style.transform = `translateY(${mobileNavStyles.getPropertyValue("height")})`;
-    mobileNavOpen = true;
-  }
-}
-
-//Close the mobile nav if the window is resized
-addEventListener("resize", (event) => {
-  if (mobileNavOpen) openMobileNav();
-});
-
-
-// !-- HEADER SCROLL ANIMATION END --!
-
-
 //for submission 
 /*
 const submission = document.getElementById("submitForum")
@@ -62,3 +11,11 @@ submission.addEventListener("click", function(){
 function onSubmit(token) {
   document.getElementById("contactForum").submit();
 }
+
+
+document.getElementById('clientRequest').addEventListener('submit', function(event) {
+  event.preventDefault();
+  var clientName = document.getElementById('clientName').value;
+  var clientEmail = document.getElementById('clientEmail').value;
+  document.getElementsByName('subject')[0].value = "New Client inquiry: " + clientName + ' (' + clientEmail + ')';
+});
