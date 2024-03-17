a12b92defe832ec2dd075873df82f600cbd0a2b2
AmritSohpal <63606966+AmritSohpal@users.noreply.github.com>
2024-03-17 17:09:11 -0400

    Update form.js

--- form.js
+++ form.js
@@ -18,3 +18,13 @@ document.getElementById('clientRequest').addEventListener('submit', function(eve
   var clientEmail = document.getElementById('clientEmail').value;
   document.getElementsByName('subject')[0].value = "New Client inquiry: " + clientName + ' (' + clientEmail + ')';
 });
+
+// features for services buttons to work 
+
+window.onload = function(){
+  var params = new URLSearchParams(window.location.search);
+  var eventType = urlParams.get('typeOfEvent');
+  if (eventType){
+    document.getElementById("typeOfEvent").value = eventType;
+  }
+};
